2a46eeceae2a03c6e688e6d97f9fab1d5549e900
El-Litios <carlosguzmanrojas00@gmail.com>
2022-05-13 14:32:19 -0400

    cambio en tamaño de imagen de slider

--- JS/Slider.js
+++ JS/Slider.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems, {
         indicators: false,
-        height: 800
+        height: 1000
     });
 });
